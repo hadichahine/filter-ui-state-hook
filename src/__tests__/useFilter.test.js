b7ae8eb2002e35e6020c65fe6e227ea06f95ba6e
Hadi Chahine <hadi.n.chahine@gmail.com>
2022-01-02 22:50:37 +0200

    Add range filter type.

--- src/__tests__/useFilter.test.js
+++ src/__tests__/useFilter.test.js
@@ -1,7 +1,7 @@
 import { act, renderHook } from "@testing-library/react-hooks";
 import { useFilter } from "../../index";
 
-test("test category filter to be unchosen initially", () => {
+test("test filter to be unchosen initially", () => {
   const { result } = renderHook(() =>
     useFilter({
       filters: [
@@ -27,7 +27,7 @@ test("test category filter to be unchosen initially", () => {
   expect(result.current.filters[0].options[0].chosen).toBe(false);
 });
 
-test("test category filter to be chosen after choosing it", () => {
+test("test filter option to be chosen after choosing option", () => {
   const { result } = renderHook(() =>
     useFilter({
       filters: [
@@ -57,7 +57,7 @@ test("test category filter to be chosen after choosing it", () => {
   expect(result.current.filters[0].options[0].chosen).toBe(true);
 });
 
-test("test category filter to be multiselect.", () => {
+test("test multiselect filter to toggle multiple options", () => {
   const { result } = renderHook(() =>
     useFilter({
       filters: [
@@ -92,7 +92,7 @@ test("test category filter to be multiselect.", () => {
   expect(result.current.filters[0].options[1].chosen).toBe(true);
 });
 
-test("test category filter to be disabled when choosen again.", () => {
+test("test filter to be disabled when choosen again", () => {
   const { result } = renderHook(() =>
     useFilter({
       filters: [
@@ -124,4 +124,103 @@ test("test category filter to be disabled when choosen again.", () => {
   });
 
   expect(result.current.filters[0].options[0].chosen).toBe(false);
+});
+
+test("test range filter to be initially empty.", () => {
+  const { result } = renderHook(() =>
+    useFilter({
+      filters: [
+        {
+          id: "category",
+          label: "Category",
+          type: "range",
+        },
+      ],
+    })
+  );
+
+  expect(result.current.filters[0].range.start).toBe(undefined);
+  expect(result.current.filters[0].range.end).toBe(undefined);
+});
+
+test("test range filter to select range", () => {
+  const { result } = renderHook(() =>
+    useFilter({
+      filters: [
+        {
+          id: "category",
+          label: "Category",
+          type: "range",
+        },
+      ],
+    })
+  );
+
+  act(() => {
+    result.current.filters[0].set({ start: 100, end: 1000 });
+  });
+
+  expect(result.current.filters[0].range.start).toBe(100);
+  expect(result.current.filters[0].range.end).toBe(1000);
+});
+
+test("test range filter to select range from options", () => {
+  const { result } = renderHook(() =>
+    useFilter({
+      filters: [
+        {
+          id: "category",
+          label: "Category",
+          type: "range",
+          options: [
+            {
+              start: 100,
+              end: 1000,
+            },
+            {
+              start: 200,
+              end: 2000,
+            },
+          ],
+        },
+      ],
+    })
+  );
+
+  act(() => {
+    result.current.filters[0].options[0].choose();
+  });
+
+  expect(result.current.filters[0].range.start).toBe(100);
+  expect(result.current.filters[0].range.end).toBe(1000);
+});
+
+test("test range filter to automatically active chosen for option when range set with 'set' is matching", () => {
+  const { result } = renderHook(() =>
+    useFilter({
+      filters: [
+        {
+          id: "category",
+          label: "Category",
+          type: "range",
+          options: [
+            {
+              start: 100,
+              end: 1000,
+            },
+            {
+              start: 200,
+              end: 2000,
+            },
+          ],
+        },
+      ],
+    })
+  );
+
+  act(() => {
+    result.current.filters[0].set({ start: 100, end: 1000 });
+  });
+
+  expect(result.current.filters[0].options[0].chosen).toBe(true);
 });
--- src/useFilter.js
+++ src/useFilter.js
@@ -5,45 +5,98 @@ export default function useFilter(options) {
 
   const [chosenFilters, setChosenFilters] = useState({});
 
-  function filter({ id: filterId, options, type, ...remaining }) {
+  function createRangeFilter(filterManifest) {
+    const { id: filterId, options = [], ...remaining } = filterManifest;
+
+    return {
+      id: filterId,
+      ...remaining,
+      set({ start, end }) {
+        setChosenFilters({
+          ...chosenFilters,
+          [filterId]: {
+            start,
+            end,
+          },
+        });
+      },
+      range: chosenFilters[filterId] ?? {},
+      options: options.map(({ start, end }) => ({
+        start,
+        end,
+        choose() {
+          setChosenFilters({
+            ...chosenFilters,
+            [filterId]: {
+              start,
+              end,
+            },
+          });
+        },
+        chosen:
+          chosenFilters[filterId]?.start === start &&
+          chosenFilters[filterId]?.end === end,
+      })),
+    };
+  }
+
+  function createMultiSelectFilter(filterManifest) {
+    const { id: filterId, options = [], ...remaining } = filterManifest;
+
     return {
       ...remaining,
       id: filterId,
-      type,
       options: options.map(({ id: optionId, ...remaining }) => ({
         ...remaining,
         id: optionId,
-        ...(type === "singleselect"
-          ? {
-              choose() {
-                setChosenFilters({
-                  ...chosenFilters,
-                  [filterId]: optionId,
-                });
-              },
-              chosen: chosenFilters[filterId] === optionId,
-            }
-          : type === "multiselect"
-          ? {
-              choose() {
-                if ((chosenFilters[filterId] ?? []).includes(optionId))
-                  setChosenFilters({
-                    ...chosenFilters,
-                    [filterId]: chosenFilters[filterId].filter(
-                      (id) => id !== optionId
-                    ),
-                  });
-                else
-                  setChosenFilters({
-                    ...chosenFilters,
-                    [filterId]: [...(chosenFilters[filterId] ?? []), optionId],
-                  });
-              },
-              chosen: (chosenFilters[filterId] ?? []).includes(optionId),
-            }
-          : { chosen: false, choose() {} }),
+        choose() {
+          if ((chosenFilters[filterId] ?? []).includes(optionId))
+            setChosenFilters({
+              ...chosenFilters,
+              [filterId]: chosenFilters[filterId].filter(
+                (id) => id !== optionId
+              ),
+            });
+          else
+            setChosenFilters({
+              ...chosenFilters,
+              [filterId]: [...(chosenFilters[filterId] ?? []), optionId],
+            });
+        },
+        chosen: (chosenFilters[filterId] ?? []).includes(optionId),
       })),
     };
+  }
+
+  function createSingleSelectFilter(filterManifest) {
+    const { id: filterId, options = [], ...remaining } = filterManifest;
+
+    return {
+      ...remaining,
+      id: filterId,
+      ...(options && {
+        options: options.map(({ id: optionId, ...remaining }) => ({
+          ...remaining,
+          id: optionId,
+          choose() {
+            setChosenFilters({
+              ...chosenFilters,
+              [filterId]: optionId,
+            });
+          },
+          chosen: chosenFilters[filterId] === optionId,
+        })),
+      }),
+    };
+  }
+
+  function filter(filterManifest) {
+    const { type } = filterManifest;
+    if (type === "range") return createRangeFilter(filterManifest);
+    if (type === "multiselect") return createMultiSelectFilter(filterManifest);
+    if (type === "singleselect")
+      return createSingleSelectFilter(filterManifest);
+    return null;
   }
 
   return {
