40df28148f63ea18b7bfa19c90d1e2ce705baf15
Hadi Chahine <hadi.n.chahine@gmail.com>
2022-01-02 14:17:42 +0200

    Implement choosing a single option from a list.

--- src/__tests__/useFilter.test.js
+++ src/__tests__/useFilter.test.js
@@ -1,7 +1,56 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { act, renderHook } from "@testing-library/react-hooks";
 import useFilter from "../useFilter";
 
-test("test nothing", () => {
-  const { result } = renderHook(useFilter);
-  expect(result.current.count).toBe(0);
+test("test category filter to be unchosen initially", () => {
+  const { result } = renderHook(() =>
+    useFilter({
+      filters: [
+        {
+          id: "category",
+          label: "Category",
+          options: [
+            {
+              id: "a",
+              label: "A",
+            },
+            {
+              id: "b",
+              label: "B",
+            },
+          ],
+        },
+      ],
+    })
+  );
+
+  expect(result.current.filters[0].options[0].chosen).toBe(false);
+});
+
+test("test category filter to be chosen after choosing it", () => {
+  const { result } = renderHook(() =>
+    useFilter({
+      filters: [
+        {
+          id: "category",
+          label: "Category",
+          options: [
+            {
+              id: "a",
+              label: "A",
+            },
+            {
+              id: "b",
+              label: "B",
+            },
+          ],
+        },
+      ],
+    })
+  );
+
+  act(() => {
+    result.current.filters[0].options[0].choose();
+  });
+
+  expect(result.current.filters[0].options[0].chosen).toBe(true);
 });
--- src/useFilter.js
+++ src/useFilter.js
@@ -1,5 +1,33 @@
-export default function useFilter() {
+import { useState } from "react";
+
+export default function useFilter(options) {
+  const { filters } = options;
+
+  const [chosenFilters, setChosenFilters] = useState({});
+
+  function choose(filterId, optionId) {
+    setChosenFilters({
+      ...chosenFilters,
+      [filterId]: optionId,
+    });
+  }
+
+  function filter({ id: filterId, options, ...remaining }) {
+    return {
+      ...remaining,
+      id: filterId,
+      options: options.map(({ id: optionId, ...remaining }) => ({
+        ...remaining,
+        id: optionId,
+        choose() {
+          choose(filterId, optionId);
+        },
+        chosen: chosenFilters[filterId] === optionId,
+      })),
+    };
+  }
+
   return {
-    count: 0,
+    filters: filters.map(filter),
   };
 }
