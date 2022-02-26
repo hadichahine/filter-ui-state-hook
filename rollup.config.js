f382eb7e01ab901b804d1711db444339e1451f31
Hadi Chahine <hadi.n.chahine@gmail.com>
2022-02-26 19:38:14 +0200

    Change build config.

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+const packageJson = require("./package.json");
+
+export default {
+  input: "main.js",
+  output: [
+    {
+      file: packageJson.main,
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: packageJson.module,
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [peerDepsExternal(), resolve(), commonjs()],
+};
